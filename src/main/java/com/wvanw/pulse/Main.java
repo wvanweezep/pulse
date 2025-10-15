0c5426043c864b248ca9d191a399493a4436ee39
Wessel van Weezep <wvanweezep@gmail.com>
2025-10-15 12:42:24 +0200

    Implemented Simple Sprite Class

--- src/main/java/com/wvanw/pulse/Main.java
+++ src/main/java/com/wvanw/pulse/Main.java
@@ -1,10 +1,12 @@
 package com.wvanw.pulse;
 
+import com.wvanw.pulse.graphics.Sprite;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 
@@ -22,6 +24,15 @@ public class Main extends Application {
         Scene scene = new Scene(root, WIDTH, HEIGHT);
         stage.setScene(scene);
         stage.show();
+
+        draw(gc);
+    }
+
+
+    private void draw(GraphicsContext gc) {
+        gc.setFill(Color.BLACK);
+        gc.fillOval(50, 50, 50, 50);
+        new Sprite("/com/wvanw/pulse/assets/Twitter-Icon.png").render(gc, 50, 50, 1, 1);
     }
 
     public static void main(String[] args) {
--- src/main/java/com/wvanw/pulse/graphics/Sprite.java
+++ src/main/java/com/wvanw/pulse/graphics/Sprite.java
@@ -1,4 +1,37 @@
 package com.wvanw.pulse.graphics;
 
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
+import java.io.InputStream;
+import java.util.HashMap;
+
 public class Sprite {
+
+    private static final String ASSETS_PATH = "/com/wvanw/pulse/assets/";
+    private static final HashMap<String, Image> CACHE = new HashMap<>();
+    private final Image sprite;
+
+    public Sprite(String path) {
+        this.sprite = CACHE.computeIfAbsent(path, Sprite::loadImage);
+    }
+
+    private static Image loadImage(String path) {
+        InputStream stream = Sprite.class.getResourceAsStream(ASSETS_PATH + path);
+        if (stream == null) throw new IllegalArgumentException(
+                "Sprite resource not found: " + path);
+        return new Image(stream);
+    }
+
+    public double getWidth() {
+        return sprite.getWidth();
+    }
+
+    public double getHeight() {
+        return sprite.getHeight();
+    }
+
+    public void render(GraphicsContext gc, int x, int y, double scaleX, double scaleY) {
+        gc.drawImage(sprite, x, y, sprite.getWidth() * scaleX, sprite.getHeight() * scaleY);
+    }
 }
